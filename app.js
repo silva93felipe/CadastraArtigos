ed26b977a07009e1378053d83266dbb34b40b68f
Felipe Santos <silva93felipe@gmail.com>
2024-09-17 12:55:07 -0300

    Tentando aplicar melhorias depois de um tempo de experiencia

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
+require("./src/db/mongoDbConnection");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const routes = require('./routes')
-
-// Permite que outros possar consumir essa API
 const cors = require('cors');
+const routes = require('./routes');
+const PORT = 8080;
 
-// Para colocar o cors para funcionar temos que criar um middleware
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*"); //O * diz que qualquer aplicação pode fazer o consumo
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); //Quais métodos podem ser usados na API
     app.use(cors());
     next();
-})
-
-// Necessário para pegar informações JSON do req.body
+});
 app.use(express.json());
-
-// Rotas do programa
 app.use(routes); 
 
-// Conexão com o bancov moongose
-mongoose.connect(process.env.ACCESS_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-}).then(() =>{
-    console.log('Conexão com o banco realizada com sucesso.');
-}).catch((err) =>{
-    console.log('Erro ao conectar ao banco:', err);
-});
-
-// Serviço do banco do back
-const PORT = 8080
-
 app.listen(PORT, () => {
-    console.log("Servidor ativo em http://localhost:8080");
+    console.log("Servidor ativo na porta 8080");
 });
--- routes.js
+++ routes.js
@@ -1,21 +1,10 @@
-const routes = require("express").Router()
-const ArtigoModel = require('./src/models/ArtigoModel');
+const routes = require("express").Router();
 const artigoController = require("./src/controllers/artigoControllers")
 
-// Rotas
-// Listar todas as informações do banco do model em questão
 routes.get("/artigos", artigoController.getAll);
-
-// Buscar um informação em específico
 routes.get('/artigos/:id', artigoController.getOne);
-
-// Editar dados no banco
 routes.put('/artigos/:id', artigoController.editar);
-
-// Deletar informações do banco
 routes.delete('/artigos/:id', artigoController.delete);
-
-// Salvar informações do banco
 routes.post('/artigos', artigoController.create);
 
 module.exports = routes;
